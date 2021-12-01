893f4be6c4e9ca421769fcc72a559bbc462facc6
Mik3Tab <miguel.talavera1995@gmail.com>
2021-12-02 00:44:15 +0100

    hay que cambiar cosas

--- js/main.js
+++ js/main.js
@@ -3,17 +3,3 @@ document.querySelector('.menu-btn').addEventListener('click', () =>{
     document.querySelector(".nav-menu").classList.toggle("show");
 })
 //menú desplegable página javascript
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
-}
